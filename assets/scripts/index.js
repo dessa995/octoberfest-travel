3605680203888ebbff4cb1f6f4c74188919e90ed
dessa995 <dessa995@outlook.com>
2022-07-29 20:44:38 +0200

    update 29 jul

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,19 +11,15 @@
 
 // findElementAndHandleEvent("fname", "Promenila se vrednost", "keyup");
 
-
 // fetch('https://jsonplaceholder.cypress.io/todos/1')
 //   .then(response => response.json())
 //   .then(function (json) {
 //     document.querySelector(".header-heading").textContent = json.title;
 //   });
 
-const submit = document.getElementById("click-button")
-  submit.addEventListener("click" function(getInputValue));
-
-
-function getInputValue(){
-  var inputVal = document.getElementById("fname").value;
-  alert(inputVal);
+function myFunction() {
+  const test = document.getElementById("fname").value;
+  console.log(test);
+  const heading = document.getElementById("header-heading");
+  heading.textContent = test;
 }
-
